caebc5941553acf459f0027ab86a2fee7187acce
YuSi Zhao <d.chaser.zsy@gmail.com>
2017-06-19 16:17:36 +0800

    bump version as same as falcon_plus

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule FalconPlusApi.Mixfile do
 
   def project do
     [app: :falcon_plus_api,
-     version: "0.1.3",
+     version: "0.2.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
